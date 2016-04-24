fc1feea5eef3dde482a1fc9598f34aa5d0a341ec
Zhang Yiwen <tiotaocn@gmail.com>
2016-04-24 15:16:58 +0800

    basic

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+});
+
+
+router.get('/exhibition', function(req, res, next) {
+	res.render('exhib', { title: '展览' });
+});
+
+router.get('/message', function(req, res, next) {
+	res.render('message',  { title: '策展人的话' });
+});
+
+router.get('/comment', function(req, res, next) {
+	res.render('comment', { title: '访客互动' });
 });
 
 module.exports = router;
