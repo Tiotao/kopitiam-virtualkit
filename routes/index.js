f4248b2c37df823930c1d11b650cbc7a765e18ba
Zhang Yiwen <yiwen@pigeonlab.com>
2016-04-28 11:55:50 +0800

    update page title

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: '欢迎 | 㗝呸店' });
 });
 
 
 router.get('/exhibition', function(req, res, next) {
-	res.render('exhib', { title: '展览' });
+	res.render('exhib', { title: '展览 | 㗝呸店' });
 });
 
 router.get('/message', function(req, res, next) {
-	res.render('message',  { title: '策展人的话' });
+	res.render('message',  { title: '策展人 | 㗝呸店' });
 });
 
 router.get('/comment', function(req, res, next) {
-	res.render('comment', { title: '访客互动' });
+	res.render('comment', { title: '访客 | 㗝呸店' });
 });
 
 router.get('/artivators', function(req, res, next) {
-	res.render('artivators', { title: '艺树人' });
+	res.render('artivators', { title: '艺树人 | 㗝呸店' });
 });
 
 
