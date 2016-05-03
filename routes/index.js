0bf7c0b08c7fcd766531fa96f007b8ea8b1c304b
Zhang Yiwen <yiwen@pigeonlab.com>
2016-05-03 12:28:46 +0800

    comment

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: '欢迎 | 㗝呸店 网上手册' });
+	res.render('index', { title: '欢迎 | 㗝呸店 - 2030年的展览' });
 });
 
 
 router.get('/exhibition', function(req, res, next) {
-	res.render('exhib', { title: '展览 | 㗝呸店 网上手册' });
+	res.render('exhib', { title: '展览 | 㗝呸店 - 2030年的展览' });
 });
 
 router.get('/message', function(req, res, next) {
-	res.render('message',  { title: '策展人 | 㗝呸店 网上手册' });
+	res.render('message',  { title: '策展人 | 㗝呸店 - 2030年的展览' });
 });
 
 router.get('/comment', function(req, res, next) {
-	res.render('comment', { title: '访客 | 㗝呸店 网上手册' });
+	res.render('comment', { title: '访客 | 㗝呸店 - 2030年的展览' });
 });
 
 router.get('/artivate', function(req, res, next) {
-	res.render('artivate', { title: '艺树人 | 㗝呸店 网上手册' });
+	res.render('artivate', { title: '艺树人 | 㗝呸店 - 2030年的展览' });
 });
 
 
